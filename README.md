fce322f368893704ae60b0c259d3724323d8e552
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2022-10-31 22:16:10 -0400

    completed a read me file, added my js code with comments

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Password_Generator
-Using JS creates a random password.
+## Description
+
+Provide a short description:
+
+-My motivation to build a password generator tat will allow me to choose criteria of my choice. I need to have an Option to select number lower and uppper case letters and Special characters. I'm building this project to further improve my knowledge of java script!
+
+## Installation
+- In order to view the code for my project, please visit https://github.com/Beaterok/Password_Generator. From there you will want to click on the green "code" button. Then you will need to copy the HTTPS link.
+
+- From there you will need to open your terminal on your computer and type "git clone "https://github.com/Beaterok/Password_Generator.git". Then type "CD Portfolio". Then type "Code ."
+
+
+
+- Below is a screen shot example of the website as well as the github repository. 
+
+![alt text](/pictures/SiteSC.png)
+![alt text](/pictures/GitSC.png)
+
+## Usage
+- You can also view the live website at https://Beaterok.github.io/Password_Generator
+
+## Credits
+Alexander Loburev.
+Jake Vanduyn.
+## License
+MIT
+## Features
+
+Let's you create a random password of your choice with a press of the button!
+
+## How to Contribute
+You can also view the live website at https://Beaterok.github.io/Password_Generator
---
+++ assets/script.js
@@ -0,0 +1,108 @@
+// Assignment code here
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters*****
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters*****
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated 
+//THEN the password is either displayed in an alert or written to the page
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+ 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+ "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ 
+var symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':',
+ ';', '<', '=', '>', '?', '@', '[', "/", '^', '_', '`', '{', "|", "}", "~"];
+ 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+ "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ 
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+ 
+ 
+ 
+ 
+var generatePassword = function () {
+ // pass is being used for all the selected variables to be stored
+ var pass = [];
+ // running an empty string for the answer (password) to be put for the user to see.
+ var strng = [];
+ 
+ var characterSelect = window.prompt("How many characters long would you like your password to be?(minimum of 8, maximum of 128 characters long)")
+ 
+ //this is allowing the character length to be between 8 and 128
+ //it is saying if user input is below 8 OR above 128 then to return null(nothing)
+ if (characterSelect < 8 || characterSelect > 128) {
+   window.alert("You must select a minimum of 8 characters, and a maximum of 128.")
+   return null
+ }
+ 
+ var symbolSelect = window.confirm("Would you like your password to have special characters?")
+ var upperSelect = window.confirm("Would you like your password to have upper case letters?")
+ var numberSelect = window.confirm("Would you like your password to have numbers?")
+ var lowerSelect = window.confirm("Would you like your password to have lowercase letters?")
+ 
+ //this is saying that if the user selects no to all requirements, it refers them to the message
+ if (symbolSelect === false && upperSelect === false && numberSelect === false && lowerSelect === false) {
+   window.alert("You must select at least one type of character.")
+   return null
+ }
+ 
+ if (symbolSelect) {
+   pass = pass.concat(symbols);
+ }
+ if (upperSelect) {
+   pass = pass.concat(upperCase);
+ }
+ if (numberSelect) {
+   pass = pass.concat(numbers);
+ }
+ if (lowerSelect) {
+   pass = pass.concat(lowerCase);
+ }
+ 
+ //for loop to generate a random character with the criteria user inputs and runs it as many times as the user inputs using "characterSelect"
+ for (var i = 0; i < characterSelect; i++) {
+   var randomCharacter = random(pass)
+   strng.push(randomCharacter)
+ }
+ //joining the answer as a string
+ return strng.join('');
+}
+//how the actual random number is selected
+function random(x) {
+ return x[Math.floor(Math.random() * x.length)]
+}
+ 
+// Write password to the #password input
+function writePassword() {
+ strng = generatePassword();
+ var passwordText = document.querySelector("#password");
+ 
+ passwordText.value = strng;
+ 
+}
+ 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+ 
+ 
+//Psuedo code below
+// max of 128 char and a min of 8
+// define all characters
+// set up the inputs for password criteria
+// use a for loop for the password length requirements
+// set up if else statement for each possible scenario for user inputs
+// Make the solution as a variable at the end(run the function at the end)
+// you need to call the answer into the HTML
+// pick a random number to select a random character from each array(use .length instead of
+//inputting how many items are in the array)
